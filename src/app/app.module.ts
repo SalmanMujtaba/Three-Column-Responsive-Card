c38a6386d3d91b4205bc9aa76439be85a39dfb25
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-02-21 16:07:14 -0500

    3 code challenge first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
